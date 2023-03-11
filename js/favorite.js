df38f47cfe7d29f84f67c66d394f2a36523ab48a
Vlad <skeimorr@gmail.com>
2023-03-11 23:12:44 +0400

    favorite counter added

--- js/favorite.js
+++ js/favorite.js
@@ -1,13 +1,16 @@
-let favoriteButton = document.querySelector('.favorite-button')
+let favoriteButton = document.querySelector('.favorite-button');
+let favoriteHeader = document.querySelector('.favorite-icon-value')
 
 favoriteButton.onclick = function (evt) {
   if (favoriteButton.classList.contains('blue-button')) {
     favoriteButton.classList.remove('blue-button');
     favoriteButton.classList.add('green-button');
     favoriteButton.textContent = 'В избранном';
+    favoriteHeader.textContent++;
   } else {
     favoriteButton.classList.remove('green-button');
     favoriteButton.classList.add('blue-button');
     favoriteButton.textContent = 'В избранное';
+    favoriteHeader.textContent--;
   }
 }
