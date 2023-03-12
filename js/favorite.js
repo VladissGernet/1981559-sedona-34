fa220545f989e7a261561860ba54db4ad43b9cae
Vlad <skeimorr@gmail.com>
2023-03-12 18:25:06 +0400

    js favorite fixed

--- js/favorite.js
+++ js/favorite.js
@@ -1,16 +1,20 @@
-let favoriteButton = document.querySelector('.favorite-button');
+let favoriteButtons = document.querySelectorAll('.favorite-button');
 let favoriteHeader = document.querySelector('.favorite-icon-value')
 
-favoriteButton.onclick = function (evt) {
-  if (favoriteButton.classList.contains('blue-button')) {
-    favoriteButton.classList.remove('blue-button');
-    favoriteButton.classList.add('green-button');
-    favoriteButton.textContent = 'В избранном';
-    favoriteHeader.textContent++;
-  } else {
-    favoriteButton.classList.remove('green-button');
-    favoriteButton.classList.add('blue-button');
-    favoriteButton.textContent = 'В избранное';
-    favoriteHeader.textContent--;
+for (let favoriteButton of favoriteButtons) {
+  favoriteButton.onclick = function (evt) {
+    if (favoriteButton.classList.contains('blue-button')) {
+      favoriteButton.classList.remove('blue-button');
+      favoriteButton.classList.add('green-button');
+      favoriteButton.textContent = 'В избранном';
+      favoriteHeader.textContent++;
+    } else {
+      favoriteButton.classList.remove('green-button');
+      favoriteButton.classList.add('blue-button');
+      favoriteButton.textContent = 'В избранное';
+      favoriteHeader.textContent--;
+    }
   }
-}
+};
+
+
